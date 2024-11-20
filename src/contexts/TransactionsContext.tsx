7dfd5fbbbcb626d7518b0e5ef67693b17c3ca014
Douglas Faria <hfsdouglas@gmail.com>
2024-11-20 00:19:57 -0300

    feat: create transactions context

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,42 @@
+import { createContext, useEffect, useState } from "react";
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  created_at: string;
+}
+
+interface TransactionsContextType {
+  transactions: Transaction[];
+}
+
+interface TransactionsProviderProps {
+  children: React.ReactNode;
+}
+
+export const TransactionsContext = createContext({} as TransactionsContextType);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3333/transactions");
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <Sem dependências>
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
