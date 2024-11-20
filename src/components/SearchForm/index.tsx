025faf547543da61508699319072c35ff23acf0b
Douglas Faria <hfsdouglas@gmail.com>
2024-11-20 01:26:28 -0300

    feat: add configs of react hook form to fields

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,11 +1,35 @@
+import { useForm } from "react-hook-form";
+import z from "zod";
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const SearchFormSchema = z.object({
+  query: z.string(),
+});
+
+type SearchFormInputs = z.infer<typeof SearchFormSchema>;
 
 export function SearchForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(SearchFormSchema),
+  });
+
+  function handleSearchTransactions(data: SearchFormInputs) {}
+
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
-      <button type="submit">
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register("query")}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
